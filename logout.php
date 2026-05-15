2e628f1b2f935ff8919e0260ae650414bcdbaf16
Othon <filhodoblues@gmail.com>
2026-05-15 18:12:34 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -7,7 +7,6 @@ if (isset($_SESSION['usuario_logado'])) {
 
 $_SESSION['sucesso_login'] = 'Você foi desconectado com sucesso.';
 
-// Redireciona para login
 header('Location: index.php');
 exit;
 ?>
